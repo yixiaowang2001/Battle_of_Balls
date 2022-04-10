9a6d09271a7173a8c5d611ae26236aca4240888a
Eric Wang <72753450+yixiaowang2001@users.noreply.github.com>
2022-04-10 13:22:09 -0500

    Update Circle

--- src/Circle.java
+++ src/Circle.java
@@ -1,21 +1,47 @@
+import java.util.List;
+import java.util.Random;
+
 import edu.macalester.graphics.*;
+import java.awt.Color;
 
 public class Circle {
     
     private CanvasWindow canvas;
 
     private static final double CIRCLE_RAIDUS = 10;
+    private static final int LOWER_BOUND = 80;
+    private static final int UPPER_BOUND = 120;
+
     private Ellipse circleShape;
     private double posX, posY;
 
     public Circle(CanvasWindow canvas) {
         this.canvas = canvas;
-
+        posX = 0;
+        posY = 0;
+        create();
     }
 
     public void create() {
-    // create a circle
-    
+        Point randPoint = createRandPos(canvas);
+        circleShape = new Ellipse(randPoint.getX(), randPoint.getY(), CIRCLE_RAIDUS, CIRCLE_RAIDUS);
+        circleShape.setFillColor(createRandColor());
+        circleShape.setStroked(false);
+        canvas.add(circleShape);
+    }
+
+    private Point createRandPos(CanvasWindow canvasWindow) {
+        Random rand = new Random();
+        double randomX = CIRCLE_RAIDUS + (canvasWindow.getWidth() - CIRCLE_RAIDUS * 2) * rand.nextDouble();
+        double randomY = CIRCLE_RAIDUS + (canvasWindow.getHeight() - CIRCLE_RAIDUS * 2) * rand.nextDouble();
+        return new Point(randomX, randomY);
+    }
+
+    private Color createRandColor() {
+        Random rand = new Random();
+        float[] hsb = Color.RGBtoHSB(rand.nextInt(255 - 0) + 0, rand.nextInt(255 - 0) + 0, rand.nextInt(255 - 0) + 0, null);
+        Color color = Color.getHSBColor(hsb[0], hsb[1], hsb[2]);
+        return color;
     }
 
     public double getX() {
@@ -26,8 +52,10 @@ public class Circle {
         return posY;
     }
 
-
     public static void main(String[] args) {
-        CanvasWindow canvas = new CanvasWindow("Test", 2000, 1000);
+        CanvasWindow canvas = new CanvasWindow("Test", 1000, 1000);
+        for (int i = 0; i < 100; i++) {
+            Circle circle = new Circle(canvas);
+        }
     }
 }
