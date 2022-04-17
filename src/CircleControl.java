83d06e2b6191d1f01aa3a714db10aaa0d9eff226
Eric Wang <72753450+yixiaowang2001@users.noreply.github.com>
2022-04-17 17:35:45 -0500

    Update

--- src/CircleControl.java
+++ src/CircleControl.java
@@ -1,16 +1,16 @@
+
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
 
 import edu.macalester.graphics.CanvasWindow;
-import edu.macalester.graphics.Point;
 
 public class CircleControl {
     
     private static final int LOWER_BOUND = 9500;
     private static final int UPPER_BOUND = 10000;
 
-    private ArrayList<Circle> circleList;
+    private List<Circle> circleList;
     private CanvasWindow canvas;
 
     public CircleControl(CanvasWindow canvas) {
@@ -19,7 +19,7 @@ public class CircleControl {
         initialize();
     }
 
-    public void initialize() {
+    private void initialize() {
         for (int i = 0; i < randInBound(); i++) {
             Circle cir = new Circle(canvas);
             circleList.add(cir);
@@ -27,9 +27,26 @@ public class CircleControl {
         }
     }
 
-    public int randInBound() {
+    public void controlNum() {
+        if (circleList.size() < LOWER_BOUND) {
+            Circle cir = new Circle(canvas);
+            circleList.add(cir);
+            cir.addToCanvas();
+        }
+    }
+
+    private int randInBound() {
         Random rand = new Random();
         return rand.nextInt(UPPER_BOUND - LOWER_BOUND) + LOWER_BOUND;
+    }
+
+    public void ifCollision(Circle cir) {
+        cir.removeFromCanvas();
+        circleList.remove(cir);
+    }
+
+    public List<Circle> getCircleList() {
+        return circleList;
     }
 
     public static void main(String[] args) {
